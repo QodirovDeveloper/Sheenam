7a01021995d7dff3d529605c8ee0f4ea3704b205
Hasanboy <QodirovDeveloper@gmail.com>
2024-09-04 00:00:27 +0500

    CODE RUB: Rename Method

--- Sheenam.Api.Tests.Unit/DeleteMe.cs
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -8,6 +8,6 @@ namespace Sheenam.Api.Tests.Unit
     public class DeleteMe
     {
         [Fact]
-        public void ShouldBeTrue() => Assert.True(true);
+        public void ShouldBeSuccessful => Assert.True(true);
     }
 }
